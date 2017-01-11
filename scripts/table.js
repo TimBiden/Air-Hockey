891da1000a7f45ad8fac4a189df5e3852fe7562b
TimBiden <timbiden@gmail.com>
2017-01-11 10:08:47 -0800

    Puck works. Need to prototype it. Beautify.

--- scripts/table.js
+++ scripts/table.js
@@ -1,10 +1,10 @@
 // Create the paddles...
-var Paddle = function(){
+var Paddle = function() {
     this.x = 0;
     this.y = 120;
 };
 
-Paddle.prototype.render = function(){
+Paddle.prototype.render = function() {
     // What in the world goes here?
 };
 
@@ -31,3 +31,24 @@ function Goal(xPoint) {
 
 computerGoal = new Goal(0);
 playerGoal = new Goal(492);
+
+var Puck = function(x, y) {
+    // variables
+    var canvas = document.getElementById('hockey');
+    var context = canvas.getContext('2d');
+    var radius = 8;
+    var startAngle = 0 * Math.PI;
+    var endAngle = 2 * Math.PI;
+    var counterClockwise = false;
+
+    // Call it all
+    context.beginPath();
+    context.arc(x, y, radius, startAngle, endAngle, counterClockwise);
+    context.lineWidth = 15;
+
+    // line color
+    context.strokeStyle = 'black';
+    context.stroke();
+};
+
+Puck(250, 150);
