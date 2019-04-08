60414b87e2027176482932327fb990fd0c27fe8d
Tim Biden <timbiden@gmail.com>
2019-04-08 15:31:19 -0700

    Buttons change successfully.

--- scripts/difficulty.js
+++ scripts/difficulty.js
@@ -1,10 +1,52 @@
 // Set game to Easy
 var level = "Medium";
 
-function difficultyEasy() {}
+function difficultyEasy() {
+  level = "Easy";
+
+  document
+    .getElementById("EasyButton")
+    .classList.remove("btn", "btn-secondary");
+  document.getElementById("EasyButton").classList.remove("btn", "btn-primary");
+  document
+    .getElementById("MediumButton")
+    .classList.remove("btn", "btn-primary");
+  document.getElementById("HardButton").classList.remove("btn", "btn-primary");
+
+  document.getElementById("EasyButton").classList.add("btn", "btn-primary");
+}
 
 // Set game to Medium
-function difficultyMedium() {}
+function difficultyMedium() {
+  level = "Medium";
+
+  document
+    .getElementById("EasyButton")
+    .classList.remove("btn", "btn-secondary");
+  document.getElementById("EasyButton").classList.remove("btn", "btn-primary");
+  document
+    .getElementById("MediumButton")
+    .classList.remove("btn", "btn-primary");
+  document.getElementById("HardButton").classList.remove("btn", "btn-primary");
+
+  document.getElementById("MediumButton").classList.add("btn", "btn-primary");
+}
 
 // Set game to Hard
-function difficultyHard() {}
+function difficultyHard() {
+  level = "Hard";
+
+  document
+    .getElementById("EasyButton")
+    .classList.remove("btn", "btn-secondary");
+  document.getElementById("EasyButton").classList.remove("btn", "btn-primary");
+  document
+    .getElementById("MediumButton")
+    .classList.remove("btn", "btn-primary");
+  document
+    .getElementById("HardButton")
+    .classList.remove("btn", "btn-secondary");
+  document.getElementById("HardButton").classList.remove("btn", "btn-primary");
+
+  document.getElementById("HardButton").classList.add("btn", "btn-primary");
+}
