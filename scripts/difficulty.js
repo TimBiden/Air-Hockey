8b88bc56e121a412036d99a99af153b489b0743c
Tim Biden <timbiden@gmail.com>
2019-04-08 15:38:31 -0700

    Clean up & notes.

--- scripts/difficulty.js
+++ scripts/difficulty.js
@@ -1,17 +1,24 @@
 // Set game to Easy
 var level = "Medium";
 
-function difficultyEasy() {
-  level = "Easy";
-
-  document
-    .getElementById("EasyButton")
-    .classList.remove("btn", "btn-secondary");
+// Remove all "btn-primary" classes.
+function removeAllPrimaries() {
   document.getElementById("EasyButton").classList.remove("btn", "btn-primary");
   document
     .getElementById("MediumButton")
     .classList.remove("btn", "btn-primary");
   document.getElementById("HardButton").classList.remove("btn", "btn-primary");
+}
+
+// Set game to Easy
+function difficultyEasy() {
+  level = "Easy";
+
+  removeAllPrimaries();
+
+  document
+    .getElementById("EasyButton")
+    .classList.remove("btn", "btn-secondary");
 
   document.getElementById("EasyButton").classList.add("btn", "btn-primary");
 }
@@ -20,14 +27,11 @@ function difficultyEasy() {
 function difficultyMedium() {
   level = "Medium";
 
-  document
-    .getElementById("EasyButton")
-    .classList.remove("btn", "btn-secondary");
-  document.getElementById("EasyButton").classList.remove("btn", "btn-primary");
+  removeAllPrimaries();
+
   document
     .getElementById("MediumButton")
-    .classList.remove("btn", "btn-primary");
-  document.getElementById("HardButton").classList.remove("btn", "btn-primary");
+    .classList.remove("btn", "btn-secondary");
 
   document.getElementById("MediumButton").classList.add("btn", "btn-primary");
 }
@@ -36,17 +40,11 @@ function difficultyMedium() {
 function difficultyHard() {
   level = "Hard";
 
-  document
-    .getElementById("EasyButton")
-    .classList.remove("btn", "btn-secondary");
-  document.getElementById("EasyButton").classList.remove("btn", "btn-primary");
-  document
-    .getElementById("MediumButton")
-    .classList.remove("btn", "btn-primary");
+  removeAllPrimaries();
+
   document
     .getElementById("HardButton")
     .classList.remove("btn", "btn-secondary");
-  document.getElementById("HardButton").classList.remove("btn", "btn-primary");
 
   document.getElementById("HardButton").classList.add("btn", "btn-primary");
 }
