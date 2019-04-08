c46faef12ce5d206b87866aa4df1d6a8b764f4fb
Tim Biden <timbiden@gmail.com>
2019-04-08 14:43:16 -0700

    Create speed settings.

---
+++ scripts/difficulty.js
@@ -0,0 +1,10 @@
+// Set game to Easy
+var level = "Medium";
+
+function difficultyEasy() {}
+
+// Set game to Medium
+function difficultyMedium() {}
+
+// Set game to Hard
+function difficultyHard() {}
