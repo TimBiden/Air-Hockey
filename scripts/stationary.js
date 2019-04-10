8f91c211d200c35046ec0ae58960cb8a2d4e4cfb
Tim Biden <timbiden@gmail.com>
2019-04-10 09:23:00 -0700

    Change opacity of lines & goals.

--- scripts/stationary.js
+++ scripts/stationary.js
@@ -1,24 +1,24 @@
 // Create Goal functions
 function Goal(xPoint) {
-  context.fillStyle = "blue";
+  context.fillStyle = "rgba(0, 51, 204, 0.6)";
   context.fillRect(xPoint, 110, 8, 80);
 }
 
 // Create Center Line
 function CenterLine() {
-  context.fillStyle = "red";
+  context.fillStyle = "rgba(255, 0, 0, 0.6)";
   context.fillRect(248, 0, 4, 300);
 }
 
 // Create Left Line
 function LeftLine() {
-  context.fillStyle = "blue";
+  context.fillStyle = "rgba(0, 51, 204, 0.6)";
   context.fillRect(160, 0, 4, 300);
 }
 
 // Create Line Line
 function RightLine() {
-  context.fillStyle = "blue";
+  context.fillStyle = "rgba(0, 51, 204, 0.6)";
   context.fillRect(328, 0, 4, 300);
 }
 
@@ -27,6 +27,6 @@ function CenterCircle() {
   context.beginPath();
   context.lineWidth = 5;
   context.arc(250, 150, 40, 0, 2 * Math.PI);
-  context.strokeStyle = "blue";
+  context.strokeStyle = "rgba(0, 51, 204, 0.6)";
   context.stroke();
 }
