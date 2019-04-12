c65f7166fdb7379b6736d3a161709841a576a545
Tim Biden <timbiden@gmail.com>
2019-04-12 08:20:39 -0700

    Player Names Change on Board

--- scripts/players.js
+++ scripts/players.js
@@ -1,9 +1,15 @@
+var numPlayers = 1;
+
 // Play against the computer
 function players1() {
+  numPlayers = 1;
+  showScore();
   console.log("1 Player");
 }
 
 // Play against another person
 function players2() {
+  numPlayers = 2;
+  showScore();
   console.log("2 Players");
 }
